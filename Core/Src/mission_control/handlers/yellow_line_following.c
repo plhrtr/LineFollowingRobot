a654b795f06e9cab2c01296b48db3b1de8eba06a
Paul Harter <pl.hrtr@gmail.com>
2026-07-08 15:18:50 +0200

    fixed non line searching init bug

--- Core/Src/mission_control/handlers/yellow_line_following.c
+++ Core/Src/mission_control/handlers/yellow_line_following.c
@@ -6,11 +6,11 @@
 // INFO: Subtract 40mm from the last distance for better sensor alignment at the
 // end
 
-static waypoint_navigation_task_t yellow_line_tasks[] = {{DRIVE_STRAIGHT, 180},
-                                                         {TURN_RIGHT, 90},
-                                                         {DRIVE_STRAIGHT, 180},
-                                                         {TURN_LEFT, 60},
-                                                         {DRIVE_STRAIGHT, 394}};
+static waypoint_navigation_task_t yellow_line_tasks[] = {{DRIVE_STRAIGHT, 425},
+                                                         {TURN_RIGHT, 150},
+                                                         {DRIVE_STRAIGHT, 250},
+                                                         {TURN_LEFT, 45},
+                                                         {DRIVE_STRAIGHT, 160}};
 
 /**
  * Callback that gets executed when the tasks are finished
